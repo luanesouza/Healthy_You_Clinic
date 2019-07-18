3cb0d1d40e5b8e7bb1fa025edf3caa66f9a3d69b
luanesouza <annie.usa25@gmail.com>
2019-07-18 17:51:57 -0400

    add index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Appointment.create(
+  [{
+    date: "July 1st",
+    notes: "I reversed the polarity of the neutron flow. I've reversed the polarity of the Neutron flow, so the TARDIS should be free from the force field now. Death is always more frightening when it strikes invisibly."
+    },
+    {
+    date: "July 2nd",
+    notes: "I reversed the polarity of the neutron flow. I've reversed the polarity of the Neutron flow, so the TARDIS should be free from the force field now. Death is always more frightening when it strikes invisibly."
+  },
+  {
+    date: "July 21st",
+    notes: "I reversed the polarity of the neutron flow. I've reversed the polarity of the Neutron flow, so the TARDIS should be free from the force field now. Death is always more frightening when it strikes invisibly."
+    },
+    {
+    date: "July 22nd",
+    notes: "I reversed the polarity of the neutron flow. I've reversed the polarity of the Neutron flow, so the TARDIS should be free from the force field now. Death is always more frightening when it strikes invisibly."
+  },]
+  )
