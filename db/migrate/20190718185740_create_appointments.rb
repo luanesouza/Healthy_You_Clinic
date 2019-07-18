3730e94362ff14266e13bebb9adc19875216dd2b
luanesouza <annie.usa25@gmail.com>
2019-07-18 14:58:12 -0400

    run rails generate resource

---
+++ db/migrate/20190718185740_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :appointments do |t|
+      t.string :date
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
