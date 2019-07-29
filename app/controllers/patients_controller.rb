76074a652da2c2dc3be0a6d2f78dde30d8c2611e
luanesouza <annie.usa25@gmail.com>
2019-07-28 20:15:53 -0400

    adds doctors, patients and appointments controller

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,18 @@
 class PatientsController < ApplicationController
+
+  def new
+    @patient = Patient.new
+  end
+
+  def create
+    @patient = Patient.create(patient_params)
+  end
+
+  private
+    def patient_params
+      params.require(:patient).permit(
+        :name,
+        :age
+      )
+    end
 end
